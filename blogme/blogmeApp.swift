81d864ccf4caeb5411c3a727d5dce78f90197bf1
Charls Puch <charlspuch@gmail.com>
2022-06-15 12:57:04 -0400

    Initial Commit

---
+++ blogme/blogmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogmeApp.swift
+//  blogme
+//
+//  Created by Carlos Pujol on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct blogmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
